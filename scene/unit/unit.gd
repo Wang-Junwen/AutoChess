a0fd22630ae8a39f67b96fa62a36b777a8e5c105
soyamilk <271450260@qq.com>
2025-12-10 01:42:54 +0800

    重构单位网格系统，添加高亮器和移动组件

--- scene/unit/unit.gd
+++ scene/unit/unit.gd
@@ -54,9 +54,9 @@ func _on_drag_started() -> void:
 
 
 func _on_drag_canceled(starting_position: Vector2) -> void:
-	reset_after_drag(starting_position)
+	reset_after_draging(starting_position)
 
 
-func reset_after_drag(starting_position: Vector2) -> void:
+func reset_after_draging(starting_position: Vector2) -> void:
 	velocity_based_rotation.enabled = false
 	global_position = starting_position
