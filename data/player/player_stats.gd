abdfa7a0a0ce616dd1b8e8c2473461ce81f60557
soyamilk <271450260@qq.com>
2025-12-11 16:53:47 +0800

    添加玩家经验值系统和升级机制

--- data/player/player_stats.gd
+++ data/player/player_stats.gd
@@ -1,18 +1,56 @@
 class_name PlayerStats
 extends Resource
 
-@export_range(0, 99) var gold: int : set = _set_gold
-@export_range(0, 99) var xp: int : set = _set_xp
-@export_range(1, 10) var level: int : set = _set_level
+const XP_REQUIREMENTS := {
+	1: 0,
+	2: 2,
+	3: 2,
+	4: 6,
+	5: 10,
+	6: 20,
+	7: 36,
+	8: 48,
+	9: 70,
+	10: 76,
+}
+
+@export_range(0, 99) var gold: int:
+	set = _set_gold
+@export_range(0, 99) var xp: int:
+	set = _set_xp
+@export_range(1, 10) var level: int:
+	set = _set_level
+
+
+func get_currentj_xp_requirement() -> int:
+	var next_level = clampi(level + 1, 1, 10)
+	return XP_REQUIREMENTS[next_level]
+
+
+func add_xp(value: int) -> void:
+	_set_xp(xp + value)
 
 
 func _set_gold(value: int) -> void:
 	gold = value
 	emit_changed()
 
+
 func _set_xp(value: int) -> void:
 	xp = value
 	emit_changed()
+
+	if level == 10:
+		return
+
+	var xp_req: int = get_currentj_xp_requirement()
+
+	while level < 10 and xp >= xp_req:
+		xp -= xp_req
+		level += 1
+		xp_req = get_currentj_xp_requirement()
+		emit_changed()
+
 
 func _set_level(value: int) -> void:
 	level = value
